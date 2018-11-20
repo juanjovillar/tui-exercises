ac1a5b66095bb9f6cb074705d9c7c80127e999f5
Juanjo Villar <juanjovillar@live.com>
2018-11-21 00:04:54 +0100

    Added Seed data and entity relations configuration

--- flight-assistant/Infrastructure/Common/CustomContext.cs
+++ flight-assistant/Infrastructure/Common/CustomContext.cs
@@ -1,7 +1,6 @@
 ﻿using Domain.Aircrafts;
 using Domain.Airports;
 using Domain.Flights;
-using GeoCoordinatePortable;
 using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure.Common
@@ -22,36 +21,69 @@ namespace Infrastructure.Common
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            configureRelations(modelBuilder);
+            seed(modelBuilder);
+        }
+
+        private void configureRelations(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Flight>()
+                .HasOne(f => f.DepartureAirport)
+                .WithMany(a => a.DepartingFlights)
+                .HasForeignKey(f => f.DepartureAirportId);
+
+            modelBuilder.Entity<Flight>()
+                .HasOne(f => f.DestinationAirport)
+                .WithMany(a => a.IncomingFlights)
+                .HasForeignKey(f => f.DepartureAirportId);
+
+            modelBuilder.Entity<Flight>()
+                .HasOne(f => f.Aircraft)
+                .WithMany(a => a.Flights)
+                .HasForeignKey(f => f.AircraftId);
+        }
+
+        private void seed(ModelBuilder modelBuilder)
+        {
             var bilbaoAirport = new Airport
             {
+                Id = 1,
                 IATA = "BIO",
                 City = "Bilbao",
-                GeoPosition = new GeoCoordinate(43.30110168457031, 2.9106099605560303)
+                Latitude = 43.30110168457031,
+                Longitude = 2.9106099605560303
             };
 
             var malagaAirport = new Airport
             {
+                Id = 2,
                 IATA = "AGP",
                 City = "Malaga",
-                GeoPosition = new GeoCoordinate(36.67490005493164, 4.499110221862793)
+                Latitude = 36.67490005493164,
+                Longitude = 4.499110221862793
             };
 
             var newYorkAirport = new Airport
             {
+                Id = 3,
                 IATA = "JFK",
                 City = "New York",
-                GeoPosition = new GeoCoordinate(40.63980103, 73.77890015)
+                Latitude = 40.63980103,
+                Longitude = 73.77890015
             };
 
             var parisAirport = new Airport
             {
+                Id = 4,
                 IATA = "CDG",
                 City = "Paris",
-                GeoPosition = new GeoCoordinate(49.0127983093, 2.54999995232)
+                Latitude = 49.0127983093,
+                Longitude = 2.54999995232
             };
 
             var airBus320 = new Aircraft
             {
+                Id = 1,
                 ModelName = "Airbus A320",
                 ConsumptionPerKm = 100,
                 ConsumptionOnTakeOff = 20
@@ -59,31 +91,21 @@ namespace Infrastructure.Common
 
             var boeing757 = new Aircraft
             {
+                Id = 2,
                 ModelName = "Boeing 757",
                 ConsumptionPerKm = 150,
                 ConsumptionOnTakeOff = 35
             };
 
-
             modelBuilder.Entity<Airport>().HasData(
                 bilbaoAirport,
                 malagaAirport,
                 newYorkAirport,
-                parisAirport
-            );
+                parisAirport);
 
             modelBuilder.Entity<Aircraft>().HasData(
                 airBus320,
-                boeing757
-            );
-
-            modelBuilder.Entity<Flight>().HasData(
-                new Flight(bilbaoAirport, malagaAirport, airBus320),
-                new Flight(bilbaoAirport, malagaAirport, boeing757),
-                new Flight(newYorkAirport, parisAirport, airBus320),
-                new Flight(bilbaoAirport, parisAirport, boeing757),
-                new Flight(malagaAirport, newYorkAirport, boeing757)
-            );
+                boeing757);
         }
     }
 }
