188bf5367a35b93a816806a011b56f2971115f4b
Juanjo Villar <juanjovillar@live.com>
2018-11-21 17:38:46 +0100

    User should be able to read a text file

---
+++ file-read/FileReader/FileReader.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO;
+
+namespace FileReader
+{
+    public class FileReader
+    {
+        public string Read(string filePath)
+        {
+            var content = File.ReadAllText(filePath);
+            return content;
+        }
+    }
+}
---
+++ file-read/FileReaderApp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+
+namespace FileReaderApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fileReader = new FileReader.FileReader();
+            var filePath = GetDefaultFilePath();
+
+            var content = fileReader.Read(filePath);
+
+            Console.Write(content);
+
+            Console.ReadLine();
+        }
+
+        private static string GetDefaultFilePath()
+        {
+            var baseDir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            var relativeFilePath = @"Files\Text\TextFile.txt";
+            var filePath = Path.Combine(baseDir, relativeFilePath);
+
+            return filePath;
+        }
+    }
+}
